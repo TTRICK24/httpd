186b8ba4049adae67cb3bb6ae94f9550973005f2
Rick Wen <rickmoon21@gmail.com>
2026-07-06 13:53:59 -0700

    Implement HTTP 404 server

--- server.c
+++ server.c
@@ -3,11 +3,42 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+
+int read_line(int fd, char *buf, size_t size)
+{
+	int i = 0;
+	char c;
+
+	while (i < size - 1) {
+		int n = read(fd, &c, 1);
+
+		if (n <= 0) {
+			break;
+		}
+
+		if (c == '\r') {
+			continue;
+		}
+
+		if (c == '\n') {
+			break;
+		}
+
+		buf[i] = c;
+		i++;
+	}
+
+	buf[i] = '\0';
+
+	return i;
+}
+
 int main()
 {
     int server_fd, client_fd;
     struct sockaddr_in addr;
     char buffer[1024];
+    char filename[256];
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -18,24 +49,30 @@ int main()
     bind(server_fd, (struct sockaddr *)&addr, sizeof(addr));
     listen(server_fd, 1);
 
-    printf("Echo server listening on port 12345...\n");
+    printf("HTTP server listening on port 12345...\n");
 
     client_fd = accept(server_fd, NULL, NULL);
 
     while (1) {
-        int n = read(client_fd, buffer, sizeof(buffer) - 1);
+        int n = read_line(client_fd, buffer, sizeof(buffer));
 
-        if (n <= 0) {
+        if (n < 0) {
             break;
         }
 
-        printf("Received: %s\n", buffer);
+	if (n == 0) {
+	    break;
+	}
 
-	if (strcmp(buffer, "exit") == 0) {
-            break;
-        }
+        printf("Line: %s\n", buffer);
 
- 	const char *response =
+	if (strncmp(buffer, "GET ", 4) == 0) {
+            sscanf(buffer, "GET %255s", filename);
+	    printf("File name: %s\n", filename);
+       	}
+    }
+
+	const char *response =
     		"HTTP/1.1 404 Not Found\r\n"
     		"Content-Type: text/plain\r\n"
     		"Content-Length: 13\r\n"
@@ -43,7 +80,7 @@ int main()
     		"404 Not Found";
 
 	write(client_fd, response, strlen(response));       
-    	}
+    	
 
     close(client_fd);
     close(server_fd);
