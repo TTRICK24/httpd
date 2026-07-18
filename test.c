dc1ac7820e7f889bb59dea909c36b38456af9c68
Rick Wen <rickmoon21@gmail.com>
2026-07-17 22:21:50 -0700

    Added user.dat

--- test.c
+++ test.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+struct user {
+	char name[64];
+	char password[64];
+};
+
+
 
 int main(int argc, char *argv[]) 
 {
-	for(int i = 0; i<argc; i++) {
-		printf("argv[%d]: %s\n", i, argv[i]);
+	if (argc != 3) {
+		printf("Usage: %s <name> <password>\n", argv[0]);
+		return 1;
 	}
+
+	struct user x;
+
+	strcpy(x.name, argv[1]);
+	strcpy(x.password, argv[2]);
+
+	FILE *f = fopen("user.dat", "wb");
+
+	if (f == NULL) {
+		printf("Cannot open file.\n");
+		return 1;
+	}
+
+	fwrite(&x, sizeof(x), 1, f);
+
+	fclose(f);
+
+	printf("User saved.\n");
 
 	return 0;
 
