6a34a6da7bb343f50984fe87e70faaf8e2f0b8c7
Rick Wen <rickmoon21@gmail.com>
2026-07-20 21:34:54 -0700

    Added show function

--- test.c
+++ test.c
@@ -10,29 +10,53 @@ struct user {
 
 int main(int argc, char *argv[]) 
 {
-	if (argc != 3) {
-		printf("Usage: %s <name> <password>\n", argv[0]);
-		return 1;
-	}
-
 	struct user x;
 
-	strcpy(x.name, argv[1]);
-	strcpy(x.password, argv[2]);
+	if (argc >= 2 && strcmp(argv[1], "add") == 0) {
 
-	FILE *f = fopen("user.dat", "wb");
+		if (argc != 4) {
+			printf("Usage: %s add <name> <password>\n", argv[0]);
+			return 1;
+		}
 
-	if (f == NULL) {
-		printf("Cannot open file.\n");
-		return 1;
+		strcpy(x.name, argv[2]);
+		strcpy(x.password, argv[3]);
+
+		FILE *f = fopen("user.dat", "wb");
+
+		if (f == NULL) {
+			printf("Cannot open file.\n");
+			return 1;
+		}
+
+		fwrite(&x, sizeof(x), 1, f);
+		fclose(f);
+
+		printf("User saved.\n");
 	}
+	else if (argc >= 2 && strcmp(argv[1], "show") == 0) {
 
-	fwrite(&x, sizeof(x), 1, f);
+		FILE *f = fopen("user.dat", "rb");
 
-	fclose(f);
+		if (f == NULL) {
+			printf("Cannot open file.\n");
+			return 1;
+		}
 
-	printf("User saved.\n");
+		fread(&x, sizeof(x), 1, f);
+		fclose(f);
+
+		printf("Name: %s\n", x.name);
+		printf("Password: %s\n", x.password);
+	}
+	else {
+		printf("Usage:\n");
+		printf(" %s add <name> <password>\n", argv[0]);
+		printf(" %s show\n", argv[0]);
+	}
 
 	return 0;
 
 }
+
+
